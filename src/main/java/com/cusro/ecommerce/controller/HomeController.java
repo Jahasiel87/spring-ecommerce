5fabeb06bdd877c7c6cc750b541d973c7497c47e
Jahasiel87 <jahasielmarchan@outlook.com>
2024-04-28 12:53:06 -0400

    redireccion a la pagina carrito

--- src/main/java/com/cusro/ecommerce/controller/HomeController.java
+++ src/main/java/com/cusro/ecommerce/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.cusro.ecommerce.model.Producto;
@@ -46,5 +47,11 @@ public class HomeController {
 		model.addAttribute("producto", producto);
 		
 		return "usuario/producto_home";
+	}
+	
+	@PostMapping("/cart")
+	public String addCart() {
+		
+		return "usuario/carrito";
 	}
 }
