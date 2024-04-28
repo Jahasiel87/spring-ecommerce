91d32f7f4eaeb1cf303ead6dd4f42dbab1bac9c3
Jahasiel87 <jahasielmarchan@outlook.com>
2024-04-28 11:28:42 -0400

    Mostrar la vista del producto al usuario

--- src/main/java/com/cusro/ecommerce/controller/HomeController.java
+++ src/main/java/com/cusro/ecommerce/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.cusro.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.cusro.ecommerce.service.IProductoService;
@@ -12,6 +15,8 @@ import com.cusro.ecommerce.service.IProductoService;
 @RequestMapping("/")
 public class HomeController {
 
+	private final Logger log=LoggerFactory.getLogger(HomeController.class);
+	
 	@Autowired
 	private IProductoService productoService;
 	
@@ -22,5 +27,13 @@ public class HomeController {
 		model.addAttribute("productos", productoService.findAll());
 		
 		return "usuario/home_usuario";
+	}
+	
+	@GetMapping("productoHome/{id}")
+	public String productoHome(@PathVariable Integer id) {
+		
+		log.info("Id producto enviado como parametro {}", id);
+		
+		return "usuario/producto_home";
 	}
 }
