335fb8c26bad65840d857de7503a88dd5e34ac89
Jahasiel87 <jahasielmarchan@outlook.com>
2024-04-29 12:36:09 -0400

    Añadir los productos al carrito de compras y mostrarlo en la vista carrito

--- src/main/java/com/cusro/ecommerce/controller/HomeController.java
+++ src/main/java/com/cusro/ecommerce/controller/HomeController.java
@@ -59,7 +59,7 @@ public class HomeController {
 	}
 	
 	@PostMapping("/cart")
-	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad) {
+	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad, Model model) {
 		
 		DetalleOrden detalleOrden= new DetalleOrden();
 		
@@ -69,9 +69,25 @@ public class HomeController {
 		
 		Optional<Producto> optionalProducto= productoService.get(id);
 		
-		log.info("Producto añadido: {}", optionalProducto.get());
-		
+		log.info("Producto añadido: {}", optionalProducto.get());		
 		log.info("Cantidad: {}", cantidad);
+		
+		producto=optionalProducto.get();
+		
+		detalleOrden.setCantidad(cantidad);
+		detalleOrden.setPrecio(producto.getPrecio());
+		detalleOrden.setNombre(producto.getNombre());
+		detalleOrden.setTotal(producto.getPrecio()*cantidad);
+		detalleOrden.setProducto(producto);
+		
+		detalles.add(detalleOrden);
+		
+		sumaTotal=detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
+		
+		orden.setTotal(sumaTotal);
+		
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
 		
 		return "usuario/carrito";
 	}
