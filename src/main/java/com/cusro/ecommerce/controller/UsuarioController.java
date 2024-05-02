d68c482bbb12d7ad0ebadac28c65cba3d431b5c9
Jahasiel87 <jahasielmarchan@outlook.com>
2024-05-02 18:59:47 -0400

    Obtener usuario login y sesión

--- src/main/java/com/cusro/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/cusro/ecommerce/controller/UsuarioController.java
@@ -1,6 +1,8 @@
 package com.cusro.ecommerce.controller;
 
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.cusro.ecommerce.model.Usuario;
 import com.cusro.ecommerce.service.IUsuarioService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/usuario")
@@ -38,4 +42,54 @@ public class UsuarioController {
 		
 		return "redirect:/";
 	}
+	
+	@GetMapping("/login")
+	public String login() {
+		
+		return "usuario/login";
+	}
+	
+	@PostMapping("/acceder")
+	public String acceder(Usuario usuario, HttpSession session) {
+		
+		logger.info("Accesos: {}", usuario);
+		
+		Optional<Usuario> user=usuarioService.findByEmail(usuario.getEmail());
+		
+		//logger.info("Usuario DB: {}", user.get());
+		
+		if(user.isPresent()) {
+			session.setAttribute("idusuario", user.get().getId());
+			
+			if(user.get().getTipo().equals("ADMIN")) {
+				
+				return "redirect:/administrador";
+								
+			}else {
+				
+				return "redirect:/";
+			  }	
+			
+			}else {
+				
+				logger.info("Usuario no existe");
+			}
+			
+		
+		
+		return "redirect:/";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
