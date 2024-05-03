0200e1fad62fc36e8625df32148461b628b23129
Jahasiel87 <jahasielmarchan@outlook.com>
2024-05-03 18:06:11 -0400

    Mostrar la vista compras para el user

--- src/main/java/com/cusro/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/cusro/ecommerce/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -73,15 +74,19 @@ public class UsuarioController {
 			}else {
 				
 				logger.info("Usuario no existe");
-			}
-			
+			}		
 		
 		
 		return "redirect:/";
 	}
 	
-	
-	
+	@GetMapping("/compras")
+	public String obtenerCompras(HttpSession session, Model model) {
+		
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
+		
+		return "usuario/compras";
+	}
 	
 	
 	
