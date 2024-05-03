43f287f17418563590e236b8aee04ce1ab654e56
Jahasiel87 <jahasielmarchan@outlook.com>
2024-05-03 19:44:15 -0400

    Cerrar la sesion del usuario

--- src/main/java/com/cusro/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/cusro/ecommerce/controller/UsuarioController.java
@@ -115,13 +115,17 @@ public class UsuarioController {
 		model.addAttribute("sesion", session.getAttribute("idusuario"));
 		
 		
-		
-		
 		return "usuario/detallecompra";
 	}
 	
 	
-	
+	@GetMapping("/cerrar")
+	public String cerrarSesion(HttpSession session) {
+		
+		session.removeAttribute("idusuario");
+		
+		return "redirect:/";
+	}
 	
 	
 	
